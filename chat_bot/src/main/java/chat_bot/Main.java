23919a67598048e57245e81f29072cd6c3c7643c
borsch <oleh.kurpiak@gmail.com>
2016-09-09 11:35:44 +0300

    init

---
+++ chat_bot/src/main/java/chat_bot/Main.java
@@ -0,0 +1,13 @@
+package chat_bot;
+
+/**
+ * Created by oleh_kurpiak on 09.09.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("A");
+    }
+
+}
+
